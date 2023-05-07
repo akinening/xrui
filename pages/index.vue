a64a12ffeafc782e54fa02e9d7babcc8dd12108c
Akinen <akinening@gmail.com>
2023-05-07 05:38:05 +0000

    jsをつけてみた

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
 
 <script>
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default {
   data() {
