8ab7c5d9311896d2b44fb38bea3ff65c4625e103
Akinen <akinening@gmail.com>
2023-05-07 05:30:49 +0000

    three.jsで表示

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+<script>
+export default {
+    setup () {
+        useHead({
+            bodyAttrs: { class: 'body-class' }
+        })
+    }
+}
+</script>
+
+<style>
+.body-class {
+    margin: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <p class="note">ⓘ 画面を左右にドラッグしてみよう</p>
+  <a class="btn" href="https://akinen.com">Akinen.com</a>
+  <div ref="threeContainer" class="three-container"></div>
+</template>
+
+<script>
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+export default {
+  data() {
+    return {
+      scene: null,
+      camera: null,
+      renderer: null,
+      controls: null,
+    };
+  },
+  mounted() {
+    this.initThree();
+  },
+  methods: {
+    initThree() {
+      const container = this.$refs.threeContainer;
+
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(renderer.domElement);
+
+      const fov = 75;
+      const aspect = container.clientWidth / container.clientHeight;
+      const near = 0.1;
+      const far = 1000;
+      const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+      camera.position.z = 1;
+
+      const scene = new THREE.Scene();
+
+      const sphereGeometry = new THREE.SphereGeometry(5, 32, 32);
+
+      const loader = new THREE.TextureLoader();
+      const texture = loader.load('https://i.ibb.co/2hx7wdY/advanced-no-style-white-minimalistic-futuristi.jpg');
+
+      const material = new THREE.MeshBasicMaterial({
+        map: texture,
+        side: THREE.BackSide,
+      });
+
+      const sphere = new THREE.Mesh(sphereGeometry, material);
+      scene.add(sphere);
+
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.enableDamping = true;
+      controls.enableZoom = false;
+      controls.dampingFactor = 0.1;
+      controls.rotateSpeed = 0.25;
+
+      const animate = () => {
+        requestAnimationFrame(animate);
+        controls.update();
+        renderer.render(scene, camera);
+      };
+
+      animate();
+    }
+  }}
+
+</script>
+
+<style scoped>
+.three-container {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.note {
+    color: #666;
+    position: absolute;
+    right: 16px;
+    text-align: center;
+    padding: 4px 16px;
+    border-radius: 99px;
+    background-color: #fff5;
+    backdrop-filter: blur(8px);
+}
+.btn {
+    position: absolute;
+    bottom: 16px;
+    left: calc(50% - 80px);
+    text-decoration: none;
+    text-align: center;
+    width: 160px;
+    padding: 16px;
+    border-radius: 99px;
+    background-color: #fff5;
+    backdrop-filter: blur(8px);
+    color: #333;
+    font-weight: bold;
+}
+
+.btn:hover {
+    color: #00aeff;
+    transition: 0.25s 
+}
+</style>
